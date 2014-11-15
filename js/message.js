ef2b896db4b84f2aa1099af7d4f312e08d996793
Marcus Noble <marcus@marcusnoble.co.uk>
2014-11-15 14:32:45 +0000

    Added message service to handle pubnub communication

---
+++ js/message.js
@@ -0,0 +1,36 @@
+var messageService = (function(){
+  var customUUID = function getCustomUUID(){
+    var uuid = sessionStorage.customUUID || PUBNUB.uuid();
+    sessionStorage.setItem('customUUID',uuid); 
+  };
+  
+  var subscribe = function subscribe(channel, onMessage){
+    onMessage = onMessage || function(message,env,channel){
+      document.getElementById('text').innerHTML += message;
+    };
+    
+    pubnub.subscribe({                                      
+      channel : channel,
+      message : onMessage
+    });
+  };
+  
+  var sendMessage = function sendMessage(channel, message, cb){
+    pubnub.publish({                                     
+         channel : channel,
+         message : message,
+         callback: cb || function(m){ console.log('Sent: ' + m) }
+    })
+  };
+  
+  pubnub = PUBNUB.init({                                  
+    publish_key   : 'pub-c-f86007fc-87f8-49f5-91b1-a2ab4ed186cc',
+    subscribe_key : 'sub-c-f6602896-6cce-11e4-b8ca-02ee2ddab7fe',
+    uuid: customUUID
+  });
+  
+  return {
+    subscribe : subscribe,
+    sendMessage : sendMessage
+  };
+}());
