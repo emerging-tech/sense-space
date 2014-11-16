a010721e133549a3a909baf2e3c07bc9660f1315
Marcus Noble <marcus@marcusnoble.co.uk>
2014-11-16 06:01:27 +0000

    Added stats service

--- README.md
+++ README.md
@@ -12,3 +12,36 @@ python -m SimpleHTTPServer
 pub-c-f86007fc-87f8-49f5-91b1-a2ab4ed186cc
 sub-c-f6602896-6cce-11e4-b8ca-02ee2ddab7fe
 ```
+
+
+## Service APIs
+
+* = optional
+
+### message service
+```javacscript
+subscribe(channel, *onMessage, *onConnect)
+sendMessage(message, *callback)
+sendLocation(location)
+tagUser(otheruser)
+getUUID
+watchParticipation(callback, *seconds)
+```
+
+### player service
+```javascript
+addPlayer(uuid)
+removePlayer(uuid)
+getPlayer(uuid)
+batchUpdate(allPaticipants)
+updatePlayerLocation(uuid, location)
+getAllPlayers()
+```
+
+### stats service
+```javasrcipt
+createStats(uuid)
+updateDistance(location)
+wasTagged()
+taggedPlayer(otherPlayer)
+```
---
+++ js/stats.js
@@ -0,0 +1,57 @@
+var statsService = (function(){
+  
+  var stats;
+  
+  function getDistanceFromLatLon(lat1,lon1,lat2,lon2) {
+    var R = 6378100; // Radius of the earth in m
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in m
+    return d;
+  }
+
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }  
+  
+  var createStats = function createStats(uuid){
+    stats.uuid = uuid;
+    stats.joined = new Date();
+    stats.tagged = [];
+  };
+  
+  var updateDistance = function updateDistance(location){
+    var oldLocation = stats.location;
+    if(oldLocation){
+      stats.distance = stats.distance + getDistanceFromLatLon(oldLocation.latitude, oldLocation.logitude, location.latitude, location.logitude);
+    }else{
+      stats.distance = 0;
+    }
+    stats.location = location;
+  };
+  
+  var wasTagged = function wasTagged(){
+    stats.tagged.push({start : new Date()});
+  };
+  
+  var taggedPlayer = function taggedPlayer(otherPlayer){
+    var lastTagged = stats.tagged[stats.tagged.length-1];
+    lastTagged.end = new Date();
+    lastTagged.duration = lastTagged.end.getTime() - lastTagged.start.getTime(); // Duration in millis
+    lastTagged.victim = otherPlayer;
+  };
+  
+  
+  return {
+    createStats : createStats,
+    updateDistance : updateDistance,
+    wasTagged : wasTagged,
+    taggedPlayer : taggedPlayer
+  };
+}());
